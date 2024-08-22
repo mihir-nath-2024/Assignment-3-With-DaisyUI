ec0e56ea0b16261b8fbb56b98cb3ae671a9b880f
mihir-nath-2024 <mihirnath114@gmail.com>
2024-08-23 01:01:34 +0600

    card section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors: {
         'custom-green': '#ABEF5F', 
         'custom-green-transparent': 'rgba(171, 239, 95, 0.5)',
+        'concept-card-1':'rgb(231, 193, 211)',
+        'concept-card-2':'rgb(239, 218, 110)',
+        'concept-card-3':'rgb(164, 218, 195)',
+        'concept-card-4':'rgb(119, 170, 234)',
       },
       
     },
